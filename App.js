9df00b8afe8bda1ba748b5fc44f937a55a78017a
Danyal <danonemun@gmail.com>
2021-07-09 23:04:53 +0200

    call API

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import FetchData from "./components/FetchData/FetchData";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <FetchData miner={"0xe17c5b456ef63903b245d9bed887f4b00f8fe654"} />
+      </ScrollView>
     </View>
   );
 }
@@ -14,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    paddingTop: 40,
   },
 });
---
+++ components/FetchData/FetchData.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Text } from "react-native";
+
+function FetchData(props = { miner: "" }) {
+  const [stats, setStats] = useState({});
+
+  const url = `https://api.ethermine.org/miner/${props.miner}/currentStats`;
+
+  const getMinerStats = () => {
+    axios
+      .get(url)
+      .then((res) => {
+        res = res.data;
+        setStats({ res });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    console.log("test");
+  };
+
+  useEffect(() => {
+    getMinerStats();
+  }, [props.miner]);
+
+  return <Text>{JSON.stringify(stats)}</Text>;
+}
+
+export default FetchData;
