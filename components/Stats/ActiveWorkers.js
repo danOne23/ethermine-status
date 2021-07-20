6c11dfeb14dc0b08dbf0b6f1d33409aed54b6d6b
Danyal <danonemun@gmail.com>
2021-07-20 19:04:23 +0200

    display zero if null

--- components/Stats/ActiveWorkers.js
+++ components/Stats/ActiveWorkers.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text } from "react-native";
 import { styles } from "./Stats";
 
 function ActiveWorkers(props = { workers: 0 }) {
+  const [activeWorkers, setActiveWorkers] = useState(props.workers);
+  if (activeWorkers == null) setActiveWorkers(0);
+
   return (
     <Text style={styles.title}>
       Active Workers{": "}
-      <Text style={styles.value}>{props.workers}</Text>{" "}
+      <Text style={styles.value}>{activeWorkers}</Text>{" "}
     </Text>
   );
 }
